490a208fd8ebb10d5d5da003b4a8b9e5964e35c5
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-29 21:02:49 +0900

    update

--- part1/src/index.js
+++ part1/src/index.js
@@ -8,8 +8,8 @@ const server = new GraphQLServer({
     type Query {
       agent(id:ID!):User!
       agents(name:String, age:Int):[User!]!
-      cars:[String!]!
-      msg(value:[String!]!):String
+      posts:[Post!]!
+      post(id:ID!): Post!
     }
     type User {
       id: ID!
@@ -18,27 +18,39 @@ const server = new GraphQLServer({
       married: Boolean!
       average: Float
     }
+    type Post {
+      id: ID!
+      title: String!
+      content: String!
+      author: User!
+    }
   `,
   resolvers: {
     Query: {
       agent: async (parent, args, context, info) => {
-        const response = await axios.get(
-          `http://localhost:3004/users/${args.id}`
-        )
+        const response = await axios.get(`${db}/users/${args.id}`)
         return response.data
       },
-      agents: async () => {
-        const response = await axios.get('http://localhost:3004/users')
+      agents: async (parent, args, context, info) => {
+        const name = args.name !== null ? `name=${args.name}` : ''
+        const age = args.age !== null ? `age=${args.age}` : ''
+        const response = await axios.get(`${db}/users?${name}&${age}`)
+
         return response.data
       },
-      cars: () => {
-        return ['Ford', null, 'Porsche']
+      posts: async (parent, args, context, info) => {
+        const response = await axios.get(`${db}/posts`)
+        return response.data
       },
-      msg: (parent, args, context, info) => {
-        if (args.values.lenght === 0) {
-          return `Sorry no value`
-        }
-        return `Hello ${args.value[0]} ${args.value[1]}`
+      post: async (parent, args, context, info) => {
+        const response = await axios.get(`${db}/posts/${args.id}`)
+        return response.data
+      }
+    },
+    Post: {
+      author: async (parent, args, context, info) => {
+        const response = await axios.get(`${db}/users/${parent.author}`)
+        return response.data
       }
     }
   }
